6a031d040b5bd44c4184e79301598cbd830a139c
sieke13 <gera.ave2@gmail.com>
2025-06-26 00:58:06 -0600

    1

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bxbsrdhxulubllidvolk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4YnNyZGh4dWx1YmxsaWR2b2xrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDkxMDY2MywiZXhwIjoyMDY2NDg2NjYzfQ.6_r9GooT8DLfN6ne_brfpKlkraGTXhUILcMnsDk7hyU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
